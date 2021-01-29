5089fc78c9a703ce85aad433f92a800b0820b9cd
Marodavi13 <mario.rodavila@gmail.com>
2021-01-29 18:56:13 +0100

    Fix includes

--- Source/LF/Game/LFGameMode.cpp
+++ Source/LF/Game/LFGameMode.cpp
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "LFGameMode.h"
-#include "LFCharacter.h"
+#include "../Actors/Character/LFCharacter.h"
 
 ALFGameMode::ALFGameMode()
 {
